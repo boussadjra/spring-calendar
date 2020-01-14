02287434b5d2f9add59384549aeb3ced7032b381
Brahim <cb_bousssadjra@esi.dz>
2020-01-14 01:21:22 +0100

    Updates

---
+++ precache-manifest.e288e48cf3d6b4306d10b95cc16ae3ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd83f7fa480045f32d635368c49da1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1903224b01132d3c8e2",
+    "url": "/static/css/main.cd0a4903.chunk.css"
+  },
+  {
+    "revision": "a9e7062e94c1bdb7a64b",
+    "url": "/static/js/2.96a056aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.96a056aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1903224b01132d3c8e2",
+    "url": "/static/js/main.3b417262.chunk.js"
+  },
+  {
+    "revision": "37397d595efa75405fde",
+    "url": "/static/js/runtime-main.4b6f9d5d.js"
+  }
+]);
