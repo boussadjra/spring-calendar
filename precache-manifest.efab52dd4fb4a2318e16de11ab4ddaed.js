615a151fdbf22ac6f29c76cc3b53dec590affd1d
Brahim <cb_bousssadjra@esi.dz>
2020-01-19 22:29:20 +0100

    Updates

---
+++ precache-manifest.efab52dd4fb4a2318e16de11ab4ddaed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bf82b11b922f8ed8793147cb840efb",
+    "url": "/spring-calendar/index.html"
+  },
+  {
+    "revision": "0b6897140474056ec390",
+    "url": "/spring-calendar/static/css/main.c7b9197e.chunk.css"
+  },
+  {
+    "revision": "a9e7062e94c1bdb7a64b",
+    "url": "/spring-calendar/static/js/2.96a056aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spring-calendar/static/js/2.96a056aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b6897140474056ec390",
+    "url": "/spring-calendar/static/js/main.64e5c364.chunk.js"
+  },
+  {
+    "revision": "fe96d328bce357ce3436",
+    "url": "/spring-calendar/static/js/runtime-main.ced4f73a.js"
+  }
+]);
